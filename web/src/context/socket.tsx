068e07c1a8b99fcdcd942547de95b15963dac113
saheenshoukath <saheenshoukath@gmail.com>
2022-10-18 21:08:57 +0530

    Update socket.tsx

--- web/src/context/socket.tsx
+++ web/src/context/socket.tsx
@@ -11,37 +11,38 @@ export const useSocket = () => {
   return React.useContext(SocketContext);
 };
 
+const connect = (url: string) => {
+  const ws = new WebSocket(url);
+  return ws;
+};
+
 const SocketProvider: React.FC<PropsWithChildren<{ url: string }>> = ({
   children,
   url,
 }) => {
-  let ws: WebSocket | null = null;
-
   const [socket, setSocket] = React.useState<WebSocket | null>(null);
 
-  const connect = () => {
-    ws = new WebSocket(url);
-
-    ws.onopen = () => {
-      console.log("Connected to websocket server");
-      setSocket(ws);
-    };
-
-    ws.onclose = () => {
-      console.log("Disconnected from websocket server");
-      setSocket(null);
-    };
-
-    ws.onmessage = (event) => {
-      if (process.env.NODE_ENV === "development") {
-        console.log("Received message from websocket server", event.data);
-      }
-    };
-  };
-
   React.useEffect(() => {
+    let ws: WebSocket | null;
+
     if (typeof window !== "undefined" && !socket) {
-      connect();
+      ws = connect(url);
+
+      ws.onopen = () => {
+        console.log("Connected to websocket server");
+        setSocket(ws);
+      };
+
+      ws.close = () => {
+        console.log("Disconnected from websocket server");
+        setSocket(null);
+      };
+
+      ws.onmessage = (event) => {
+        if (process.env.NODE_ENV === "development") {
+          console.log("Received message from websocket server", event.data);
+        }
+      };
     }
 
     return () => {
