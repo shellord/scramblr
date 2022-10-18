283945849a1e7ea985d6777cb969e49f496d0f3c
Saheen Shoukath <saheenshoukath@gmail.com>
2022-10-18 21:35:03 +0530

    update socket.ts

--- web/src/context/socket.tsx
+++ web/src/context/socket.tsx
@@ -15,33 +15,29 @@ const SocketProvider: React.FC<PropsWithChildren<{ url: string }>> = ({
   children,
   url,
 }) => {
-  let ws: WebSocket | null = null;
-
   const [socket, setSocket] = React.useState<WebSocket | null>(null);
 
-  const connect = () => {
-    ws = new WebSocket(url);
-
-    ws.onopen = () => {
-      console.log("Connected to websocket server");
-      setSocket(ws);
-    };
-
-    ws.onclose = () => {
-      console.log("Disconnected from websocket server");
-      setSocket(null);
-    };
-
-    ws.onmessage = (event) => {
-      if (process.env.NODE_ENV === "development") {
-        console.log("Received message from websocket server", event.data);
-      }
-    };
-  };
-
   React.useEffect(() => {
+    let ws: WebSocket | null;
+
     if (typeof window !== "undefined" && !socket) {
-      connect();
+      ws = new WebSocket(url);
+
+      ws.onopen = () => {
+        console.log("Connected to websocket server");
+        setSocket(ws);
+      };
+
+      ws.close = () => {
+        console.log("Disconnected from websocket server");
+        setSocket(null);
+      };
+
+      ws.onmessage = (event) => {
+        if (process.env.NODE_ENV === "development") {
+          console.log("Received message from websocket server", event.data);
+        }
+      };
     }
 
     return () => {
